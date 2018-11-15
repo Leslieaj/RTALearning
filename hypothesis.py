4a25d9b0c5bb0d28a165e87aa81f4e19266e24d9
leslie <ajstar1990@sina.com>
2018-11-15 18:54:04 +0800

    fix a bug in building an evidence automaton and move EA definitions and these functions into a new file hypothesis.py

---
+++ hypothesis.py
@@ -0,0 +1,151 @@
+#define Evidence Automaton and build a hypothesis RTA
+
+from membership import *
+
+class EvidenceAutomaton():
+    def __init__(self, sigma= None, states=None, trans=None, initstate=None, accept=None):
+        #self.name = name
+        self.sigma = sigma
+        self.states = states or []
+        self.trans = trans or []
+        self.initstate_name = initstate or []
+        self.accept_names = accept or []
+    
+    def show(self):
+        #print "EA name: "
+        #print self.name
+        print "sigma and length of sigma: "
+        print self.sigma, len(self.sigma)
+        print "State (name, init, accept) :"
+        for s in self.states:
+            print s.name, s.init, s.accept
+        print "transitions (id, source_state, label, target_state): "
+        for t in self.trans:
+            print t.id, t.source, [tw.show() for tw in t.label], t.target
+            print
+        print "init state: "
+        print self.initstate_name
+        print "accept states: "
+        print self.accept_names
+
+class EATran():
+    def __init__(self, id, source = "", label = None, target = ""):
+        self.id = id
+        self.source = source
+        self.label = label or []
+        self.target = target
+
+def buildEvidenceAutomaton(table, sigma):
+    new_sigma = [action for action in sigma]
+    states = []
+    initstate_name = ""
+    accept_names = []
+    for s in table.S:
+        name = s.whichstate()
+        init = False
+        accept = False
+        if s.tws == []:
+            init = True
+            initstate_name = name
+        if s.value[0] == 1:
+            accept = True
+            accept_names.append(name)
+        temp_state = State(name, init, accept)
+        states.append(temp_state)
+    trans_number = 0
+    trans = []
+    table_element = [s for s in table.S] + [r for r in table.R]
+    for r in table_element:
+        if r.tws == []:
+            continue
+        timedwords = [tw for tw in r.tws]
+        w = timedwords[:-1]
+        a = timedwords[len(timedwords)-1]
+        source = ""
+        target = ""
+        label = [a]
+        for element in table_element:
+            if w == element.tws:
+                source = element.whichstate()
+            if timedwords == element.tws:
+                target = element.whichstate()
+        need_newtran = True
+        for tran in trans:
+            if source == tran.source and target == tran.target:
+                if a.action == tran.label[0].action:
+                    if a in tran.label:
+                        need_newtran = False
+                    else:
+                        tran.label.append(a)
+                        need_newtran = False
+        if need_newtran == True:
+            temp_tran = EATran(trans_number, source, label, target)
+            trans.append(temp_tran)
+            trans_number = trans_number + 1
+    ea = EvidenceAutomaton(new_sigma, states, trans, initstate_name, accept_names)
+    return ea
+
+def main():
+    A = buildRTA("a.json")
+    AA = buildAssistantRTA(A)
+    sigma = ["a", "b"]
+
+    tw1 = Timedword("a", 0)
+    tw2 = Timedword("b", 0)
+    tw3 = Timedword("a", 5)
+    tw4 = Timedword("b", 4)
+    tw5 = Timedword("a", 7)
+    tw6 = Timedword("b", 2)
+    tws0 = [] # empty
+    tws1 = [tw1] # (a,0)
+    tws2 = [tw2] # (b,0)
+    tws3 = [tw3] # (a,5)
+    tws4 = [tw3,tw1] # (a,5) (a,0)
+    tws5 = [tw3,tw2] # (a,5) (b,0)
+    tws6 = [tw5] # (a,7)
+    tws7 = [tw4] # (b,4)
+    tws8 = [tw4,tw3] # (b,4) (a,5)
+    tws9 = [tw1,tw1] # (a,0) (a,0)
+    tws10 = [tw1,tw2] # (a,0) (b,0)
+    tws11 = [tw6,tw3] # (b,2) (a,5)
+    tws12 = [tw6] #(b,2)
+
+    e0 = Element(tws0,[0])
+    e1 = Element(tws1,[0])
+    e2 = Element(tws2,[0])
+    e3 = Element(tws3,[1])
+    e4 = Element(tws4,[0,1,0,1])
+    e5 = Element(tws5,[1,1,0,1])
+    S = [e0]
+    R = [e1,e2,e3]
+    E = []
+    T = Table(S,R,E)
+    
+    T3 = make_closed(T, sigma, AA)
+    T3.show()
+    
+    ctx2 = tws6
+    T4 = add_ctx(T3, ctx2, AA)
+    T4.show()
+    print("----------------------T5--------------------------")
+    ctx3 = tws8
+    T5 = add_ctx(T4, ctx3, AA)
+    T5.show()
+    print("----------------------T6--------------------------")
+    T6 = make_consistent(T5, sigma, AA)
+    T6.show()  
+    print("----------------------T7--------------------------")
+    T7 = make_closed(T6, sigma, AA)
+    T7.show()
+    print("----------------------T8--------------------------")
+    ctx4 = tws11
+    T8 = add_ctx(T7, ctx4, AA)
+    T8.show()
+    print("----------------------EA--------------------------")
+    ea = buildEvidenceAutomaton(T8, sigma)
+    ea.show()
+
+    return 0
+
+if __name__=='__main__':
+	main()
